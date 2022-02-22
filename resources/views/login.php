1c59cb38987484f417d59caf1392bb3d241ead57
ridwanchowdhury <ridroxx@gmail.com>
2022-02-23 00:06:11 +0600

    nothing changed. this is just a try

---
+++ resources/views/login.php
@@ -0,0 +1,7 @@
+<html>
+    <form action="" method="">
+        <input type="text" placeholder="Username"> <br>
+        <input type="text" placeholder="Password"><br>
+        <input type="submit" value= "login">
+    </form>
+</html>
